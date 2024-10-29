575cbeec3572d55c4aff99df93ee2731e50f3fce
moonprincelol <patrickwang2003@gmail.com>
2024-10-29 15:45:53 +0800

    content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faFreeCodeCamp, faPython, faJs, faReact, faGithub } from '@fortawesome/free-brands-svg-icons';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Patrick Wang</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Information Science student at Utrecht University, The Netherlands.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+        <div className="links">
+          <a class="linkedin" href="https://www.linkedin.com/in/patrick-wang-b42702294/" target="_blank_">
+            <FontAwesomeIcon icon={faLinkedin} />
         </a>
+
+          <a class="github" href="https://github.com/moonprincelol" target="_blank_">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+
+        <div className="row">
+        <div className="column">
+        <div className="skills">
+          <h2>Skills</h2>
+          <p className="pythontext"><FontAwesomeIcon icon={faPython} />Python</p>
+          <p className='jstext'><FontAwesomeIcon icon={faJs}/>JavaScript</p>
+          <p className="reacttext"><FontAwesomeIcon icon={faReact}/>React</p>
+        </div>
+        </div>
+        <div className="column">
+        <div className="projects">
+        <h2>Projects</h2>
+        <h3 className="freecodecamp">freeCodeCamp <FontAwesomeIcon icon={faFreeCodeCamp}/></h3>
+        <hr></hr>
+        <p className="project">
+          <a href="https://moonprincelol.github.io/randomQuoteMachine" target="_blank_">Random Quote Machine</a>
+        </p>
+        <p className="project">
+          <a href="https://moonprincelol.github.io/markdown-previewer"  target="_blank_">Markdown Previewer</a>
+        </p>
+
+        <p className="project">
+          <a href="https://moonprincelol.github.io/drum-machine"  target="_blank_">Drum Machine</a>
+        </p>
+
+        <p className="project">
+          <a href="https://moonprincelol.github.io/js-calculator/"  target="_blank_">Calculator</a>
+        </p>
+        <p className="project">
+          <a href="https://moonprincelol.github.io/25plus5-clock"  target="_blank_">Pomodoro Clock</a>
+        </p>
+        </div>
+        </div>
+        </div>
       </header>
     </div>
   );
